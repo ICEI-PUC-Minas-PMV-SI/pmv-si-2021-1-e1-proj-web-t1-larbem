e0d1e07d4711625501e221ac24c1bae289e0a180
Matheus Andrade <devmatheusandrade@gmail.com>
2021-06-11 05:14:41 -0300

    Add menu hamburguer

--- src/script.js
+++ src/script.js
@@ -2,3 +2,8 @@ window.addEventListener("scroll", function () {
     var navbar = document.getElementById("navbar");
     navbar.classList.toggle("sticky", window.scrollY > 0);
 })
+
+function onClickMenu(){
+    document.getElementById("menu").classList.toggle("change");
+    document.getElementById("list").classList.toggle("change");
+}
